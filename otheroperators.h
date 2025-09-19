fd96d974273e897710db1fe4088d16c30f3ed3e3
ny-julian-g <julian.gasser@noseryoung.ch>
2025-09-19 16:01:28 +0200

    Created the Diffrent sections

---
+++ otheroperators.h
@@ -0,0 +1,8 @@
+//
+// Created by jgasser on 19.09.2025.
+//
+
+#ifndef CALC_TEAM8_OTHEROPERATORS_H_H
+#define CALC_TEAM8_OTHEROPERATORS_H_H
+#include "otheroperators.h"
+#endif //CALC_TEAM8_OTHEROPERATORS_H_H
