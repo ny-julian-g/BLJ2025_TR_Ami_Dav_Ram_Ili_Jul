ecef9cc915a209f551a6f148aab53343eec4c858
ny-amina-a <amina.abdurehim@noseryoung.ch>
2025-09-19 16:42:31 +0200

    file erstellt

---
+++ calc_08.c
@@ -0,0 +1,5 @@
+//
+// Created by Startklar on 19.09.2025.
+//
+
+#include "calc_08.h"
---
+++ calc_08.h
@@ -0,0 +1,8 @@
+//
+// Created by Startklar on 19.09.2025.
+//
+
+#ifndef CALC_TEAM8_CALC_08_H
+#define CALC_TEAM8_CALC_08_H
+
+#endif //CALC_TEAM8_CALC_08_H
