0945c56817c975b62575d21e9ba530b73f4086ee
ny-julian-g <julian.gasser@noseryoung.ch>
2025-09-24 14:03:22 +0200

    Start with logic in main

--- main.c
+++ main.c
@@ -5,7 +5,11 @@ int grundrechner();
 double exponential(double inputa,double inputb);
 double calcsqrt(double inputa);
 double faculty(double inputa);
-
+int hello();
 void main() {
-
+ int choice = hello();
+ switch (choice) {
+  case 1:
+   grundrechner();
+ }
 }
