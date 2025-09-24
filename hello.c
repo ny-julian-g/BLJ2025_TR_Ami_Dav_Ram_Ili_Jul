aea1d561c8f644d08c9ee8e8bc215763dc8dcb1e
ny-david-s <david.stupar@noseryoung.com>
2025-09-24 16:40:24 +0200

    Update hello.c

--- hello.c
+++ hello.c
@@ -2,12 +2,41 @@
 #include "hello.h"
 #include <stdio.h>
 
+#include "calc_08.h"
+#include "otheroperators.h"
+
 int hello() {
+    int auswahl = 0;
     printf("Hello\n\n");
-    printf("1 -> Standard rechner\n");
+    printf("1 -> Standardrechner\n");
     printf("2 -> Potenz\n");
-        printf("3 -> Wurzel\n");
-        printf("4 -> Fakultät\n");
+    printf("3 -> Wurzel\n");
+    printf("4 -> Fakultaet\n");
     printf("5 -> Rock, Paper, Scissors\n");
-    return 0;
+
+    scanf("%d", auswahl);
+}
+switch(auswahl) {
+    case 1:
+        grundrechner();
+        break;
+    case 2:
+        exponential(1,1);
+        break;
+    case 3:
+        calcsqrt();
+        break;
+    case 4:
+        faculty();
+        break;
+    /*case 5:
+        spiel();
+        break;*/
+    default:
+        printf("Ungueltige Eingabe!\n");
+
+return 0;
+
+
+
 }
